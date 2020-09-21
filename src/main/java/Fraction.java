80a5d2b78d6c0cf05a56d0d31537e87a1dd4808e
Daniel Suda <daniel.suda@educanet.cz>
2020-09-21 20:58:01 +0200

    hotovo!

--- src/main/java/Fraction.java
+++ src/main/java/Fraction.java
@@ -10,40 +10,56 @@ public class Fraction implements IFraction {
         if (denominator == 0) throw new ArithmeticException("Cannot Divide by 0");
     }
 
+    private Fraction reduceFraction(int numerator, int denominator) {
+        int d;
+        d = greatestCommonDivisor(numerator, denominator);
+
+        numerator = numerator / d;
+        denominator = denominator / d;
+        return new Fraction(numerator, denominator);
+    }
+
+    private int greatestCommonDivisor(int a, int b) {
+        if (b == 0)
+            return a;
+        return greatestCommonDivisor(b, a % b);
+    }
+
     @Override
-    public Integer getNumerator() {
+    public @NotNull
+    Integer getNumerator() {
         return numerator;
     }
 
     @Override
-    public Integer getDenominator() {
+    public @NotNull
+    Integer getDenominator() {
         return denominator;
     }
 
     @Override
-    public IFraction plus(IFraction other) {
-        throw new UnsupportedOperationException();
+    public @NotNull
+    IFraction plus(@NotNull IFraction other) {
+        return reduceFraction((numerator * other.getDenominator()) + (denominator * other.getNumerator()), denominator * other.getDenominator());
     }
 
     @Override
-    public IFraction minus(IFraction other) {
-        throw new UnsupportedOperationException();
+    public @NotNull
+    IFraction minus(@NotNull IFraction other) {
+        return reduceFraction((numerator * other.getDenominator()) - (denominator * other.getNumerator()), denominator * other.getDenominator());
     }
 
     @Override
-    public IFraction times(IFraction other) {
-        throw new UnsupportedOperationException();
+    public @NotNull
+    IFraction times(@NotNull IFraction other) {
+        return reduceFraction(this.numerator * other.getNumerator(), this.denominator * other.getDenominator());
     }
 
     @Override
-    public IFraction dividedBy(IFraction other) {
-        throw new UnsupportedOperationException();
+    public @NotNull
+    IFraction dividedBy(@NotNull IFraction other) {
+        return reduceFraction(this.numerator * other.getDenominator(), this.denominator * other.getNumerator());
     }
-
-    public static Fraction createNormalised(Integer numerator, Integer denominator) {
-        throw new UnsupportedOperationException();
-    }
-
     @Override
     public String toString() {
         return "Fraction " + numerator + "|" + denominator;
--- src/test/java/TestAddition.java
+++ src/test/java/TestAddition.java
@@ -14,6 +14,60 @@ public class TestAddition {
     }
 
     @Test
+    public void testDividedBySimple() {
+        var fraction = TestUtils.createFraction(4, 1);
+        var result = fraction.dividedBy(new NotImplementedFraction(3, 1));
+
+        Assert.assertEquals((Integer) 4, result.getNumerator());
+        Assert.assertEquals((Integer) 3, result.getDenominator());
+    }
+
+    @Test
+    public void testDividedByNormalisation() {
+        var fraction = TestUtils.createFraction(4, 6);
+        var result = fraction.dividedBy(new NotImplementedFraction(7, 5));
+
+        Assert.assertEquals((Integer) 10, result.getNumerator());
+        Assert.assertEquals((Integer) 21, result.getDenominator());
+    }
+
+    @Test
+    public void testTimesSimple() {
+        var fraction = TestUtils.createFraction(2, 3);
+        var result = fraction.times(new NotImplementedFraction(5, 7));
+
+        Assert.assertEquals((Integer) 10, result.getNumerator());
+        Assert.assertEquals((Integer) 21, result.getDenominator());
+    }
+
+    @Test
+    public void testTimesNormalisation() {
+        var fraction = TestUtils.createFraction(4, 6);
+        var result = fraction.times(new NotImplementedFraction(5, 7));
+
+        Assert.assertEquals((Integer) 10, result.getNumerator());
+        Assert.assertEquals((Integer) 21, result.getDenominator());
+    }
+
+    @Test
+    public void testMinusSimple() {
+        var fraction = TestUtils.createFraction(7, 9);
+        var result = fraction.minus(new NotImplementedFraction(2, 5));
+
+        Assert.assertEquals((Integer) 17, result.getNumerator());
+        Assert.assertEquals((Integer) 45, result.getDenominator());
+    }
+
+    @Test
+    public void testMinusNormalisation() {
+        var fraction = TestUtils.createFraction(6, 4);
+        var result = fraction.minus(new NotImplementedFraction(1, 2));
+
+        Assert.assertEquals((Integer) 1, result.getNumerator());
+        Assert.assertEquals((Integer) 1, result.getDenominator());
+    }
+
+    @Test
     public void testNormalisation() {
         var fraction = TestUtils.createFraction(1, 2);
         var result = fraction.plus(new NotImplementedFraction(1, 2));
